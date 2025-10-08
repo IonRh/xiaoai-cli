50bfcda2fd0482e4f18f644463e3179abb5c39aa
DreamAlone <dalone666@qq.com>
2025-10-08 20:08:23 +0800

    [cli] 添加 `device` 子命令

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,14 +1,16 @@
-use std::{fs::File, path::PathBuf};
+use std::{fs::File, io::BufReader, path::PathBuf};
 
 use clap::{Parser, Subcommand};
 use inquire::{Confirm, Password, PasswordDisplayMode, Text};
 use miai::Xiaoai;
 
+const DEFAULT_AUTH_FILE: &str = "xiaoai-auth.json";
+
 #[derive(Parser)]
 #[command(version, about)]
 struct Cli {
     #[command(subcommand)]
-    command: Option<Commands>,
+    command: Commands,
 }
 
 #[derive(Subcommand)]
@@ -16,8 +18,14 @@ enum Commands {
     /// 登录小爱服务以获得认证
     Login {
         /// 另存为认证文件
-        #[arg(short, long, default_value = "xiaoai-auth.json")]
+        #[arg(short, long, default_value = DEFAULT_AUTH_FILE)]
         save: PathBuf,
+    },
+    /// 列出小爱设备
+    Device {
+        /// 加载认证文件
+        #[arg(long, default_value = DEFAULT_AUTH_FILE)]
+        auth: PathBuf,
     },
 }
 
@@ -25,28 +33,36 @@ enum Commands {
 async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
-    if let Some(command) = cli.command {
-        match command {
-            Commands::Login { save } => {
-                let username = Text::new("账号:").prompt()?;
-                let password = Password::new("密码:")
-                    .with_display_toggle_enabled()
-                    .with_display_mode(PasswordDisplayMode::Masked)
-                    .without_confirmation()
-                    .with_help_message("CTRL + R 显示/隐藏密码")
-                    .prompt()?;
-                let xiaoai = Xiaoai::login(&username, &password).await?;
+    match cli.command {
+        Commands::Login { save } => {
+            let username = Text::new("账号:").prompt()?;
+            let password = Password::new("密码:")
+                .with_display_toggle_enabled()
+                .with_display_mode(PasswordDisplayMode::Masked)
+                .without_confirmation()
+                .with_help_message("CTRL + R 显示/隐藏密码")
+                .prompt()?;
+            let xiaoai = Xiaoai::login(&username, &password).await?;
 
-                let can_save = if save.exists() {
-                    Confirm::new(&format!("{} 已存在，是否覆盖?", save.display())).prompt()?
-                } else {
-                    true
-                };
+            let can_save = if save.exists() {
+                Confirm::new(&format!("{} 已存在，是否覆盖?", save.display())).prompt()?
+            } else {
+                true
+            };
 
-                if can_save {
-                    let mut file = File::create(save)?;
-                    xiaoai.save(&mut file).map_err(anyhow::Error::from_boxed)?;
-                }
+            if can_save {
+                let mut file = File::create(save)?;
+                xiaoai.save(&mut file).map_err(anyhow::Error::from_boxed)?;
+            }
+        }
+        Commands::Device { auth } => {
+            let file = File::open(auth)?;
+            let xiaoai = Xiaoai::load(BufReader::new(file)).map_err(anyhow::Error::from_boxed)?;
+            let devices = xiaoai.device_info().await?;
+            for device in devices {
+                println!("名称: {}", device.name);
+                println!("设备 ID: {}", device.device_id);
+                println!("机型: {}", device.hardware);
             }
         }
     }
